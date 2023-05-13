0af38d804a7df48b57fde38b07924ea48e68d318
klstxii <ksarmiento@student.hau.edu.ph>
2023-05-13 21:45:37 +0800

    product details

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {Nav, Navbar, NavDropdown, Button, Form, Input, Container} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import {Nav, Navbar, Container} from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
     <Navbar bg="primary" expand="lg" variant='dark' collapseOnSelect>
       <Container>
-        <Navbar.Brand href='/'>Pokemart</Navbar.Brand>
+        <LinkContainer to='/'>
+        <Navbar.Brand><i className='fas fa-shop'></i> Pokemart</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
           <Nav className='ml-auto'>
-            <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-            <Nav.Link href='/login'><i className='fas fa-user'></i> Sign in</Nav.Link>
+            <LinkContainer to='/cart'>
+              <Nav.Link><i className='fas fa-shopping-cart'></i></Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Nav.Link><i className='fas fa-user'></i></Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
