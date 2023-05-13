5ed79bce46f8dcafb4733206f701bd7f781cafff
klstxii <ksarmiento@student.hau.edu.ph>
2023-05-13 17:34:30 +0800

    products card

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+import products from '../products';
+import Product from '../components/Product';
+
+const Homescreen = () => {
+  return (
+    <div>
+        <h1>Available Products</h1>
+        <Row>
+            {products.map(product => (
+                <Col sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
+                </Col>
+
+            ))}
+        </Row>
+    </div>
+  )
+}
+
+export default Homescreen
