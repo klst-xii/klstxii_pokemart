6ef06a338e23fd4c8edd4d5e37cbc9b91815e575
klstxii <ksarmiento@student.hau.edu.ph>
2023-05-19 21:42:33 +0800

    register view

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
     path('', views.getRoutes, name="Routes"),
     path('users/profile/', views.getUserProfile, name="Users-Profile"),
     path('users/', views.getUsers, name="Users"),
