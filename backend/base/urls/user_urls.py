b92909d5e86e15ae292ecca56ca7e3e387d57af7
klstxii <ksarmiento@student.hau.edu.ph>
2023-05-25 14:42:16 +0800

    success error

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name="Users-Profile"),
+    path('profile/update/', views.updateUserProfile, name="Users-Profile-Update"),
     path('', views.getUsers, name="Users"),
 ]
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {LinkContainer} from 'react-router-bootstrap';
-import {Nav, Navbar, Container} from 'react-bootstrap';
+import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';
+import { logout } from '../actions/userActions';
 
-const Header = () => {
+
+function Header() {
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
     <Navbar bg="primary" expand="lg" variant='dark' collapseOnSelect>
@@ -13,12 +26,27 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
           <Nav className='ml-auto'>
+
             <LinkContainer to='/cart'>
               <Nav.Link><i className='fas fa-shopping-cart'></i></Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/login'>
-              <Nav.Link><i className='fas fa-user'></i></Nav.Link>
-            </LinkContainer>
+
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='username'>
+                <LinkContainer to='/profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+
+                <NavDropdown.Item onClick={logoutHandler} >Logout</NavDropdown.Item>
+
+              </NavDropdown>
+            ) : (
+                    <LinkContainer to='/login'>
+                      <Nav.Link><i className='fas fa-user'></i></Nav.Link>
+                    </LinkContainer>
+                )}
+
+
           </Nav>
         </Navbar.Collapse>
       </Container>
